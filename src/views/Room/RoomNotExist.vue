85ee67e921f83f10a62584522a58ee0f73a25981
RafalFak <rafalfak8@gmail.com>
2024-01-21 11:48:26 +0100

    feat(Room): RoomNotExist

---
+++ src/views/Room/RoomNotExist.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container">
+    <HeaderCard>Something went wrong! Such a room doesn't exist.</HeaderCard>
+    <Button @click="goHome">
+      <v-icon icon="mdi-exit-run" size="large" />
+      Go Home
+    </Button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Button, HeaderCard } from "@/components";
+import { useRouter } from "vue-router";
+
+import { RoutesNames } from "@/router";
+
+const router = useRouter();
+
+const goHome = () => {
+  router.push({ name: RoutesNames.Home });
+};
+</script>
+
+<style lang="scss">
+.container {
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
