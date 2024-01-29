494a6cd2468765b6cbb1e385d9ae5923ae02381b
RafalFak <rafalfak8@gmail.com>
2024-01-29 14:35:31 +0100

    feat(RoomView): open-menu-button

---
+++ src/components/ButtonIcon.vue
@@ -0,0 +1,34 @@
+<template>
+  <Button icon :class="{ rotate }"><v-icon :icon="icon" size="large" /></Button>
+</template>
+
+<script setup lang="ts">
+import Button from "./Button.vue";
+
+defineProps({
+  icon: {
+    type: String,
+    required: true,
+  },
+  rotate: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.v-btn--icon {
+  height: 38px;
+  width: 38px;
+  padding-top: 1px;
+
+  &.rotate {
+    transform: rotate(180deg);
+  }
+
+  &--icon {
+    width: 38px;
+  }
+}
+</style>
