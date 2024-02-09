d060873baa12e39947cc98712516dfcb66d437c1
RafalFak <rafalfak8@gmail.com>
2024-02-09 15:43:24 +0100

    feat(ExtensionPanelSingle): init

---
+++ src/components/ExtensionPanelSingle.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-expansion-panels>
+    <v-expansion-panel>
+      <v-expansion-panel-title>
+        {{ title }}
+      </v-expansion-panel-title>
+      <v-expansion-panel-text>
+        <slot></slot>
+      </v-expansion-panel-text>
+    </v-expansion-panel>
+  </v-expansion-panels>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped></style>
