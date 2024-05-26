f8be880f026310657339f9d56d3e48a8ebc7f325
RafalFak <rafalfak8@gmail.com>
2024-05-26 09:51:46 +0200

    feat(OptionsResult): handling the same number of points in result

--- src/components/OptionsResult.vue
+++ src/components/OptionsResult.vue
@@ -2,8 +2,8 @@
   <OptionCard
     v-for="({ option, points }, index) in ranking"
     :key="option"
-    :ordinalNumber="index + 1"
-    :big="index === 0"
+    :ordinalNumber="ordinalNumbersMap[index]"
+    :big="ordinalNumbersMap[index] === 1"
   >
     <template v-slot:leftColumn>
       {{ option }}
@@ -20,7 +20,22 @@ import { PropType } from "vue";
 import { OptionCard } from "@/components";
 import { ResultOption } from "@/types";
 
-defineProps({
-  ranking: Array as PropType<ResultOption[]>,
+const props = defineProps({
+  ranking: {
+    type: Array as PropType<ResultOption[]>,
+    required: true,
+  },
+});
+
+const ordinalNumbersMap: number[] = [];
+
+props.ranking.forEach(({ points }, index, array) => {
+  if (index === 0) {
+    return ordinalNumbersMap.push(1);
+  }
+  if (points === array[index - 1].points) {
+    return ordinalNumbersMap.push(ordinalNumbersMap[index - 1]);
+  }
+  return ordinalNumbersMap.push(ordinalNumbersMap[index - 1] + 1);
 });
 </script>
